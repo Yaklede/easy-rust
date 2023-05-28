5382972eadc34194f16caefadd408d063a31a826
Yaklede <68309983+Yaklede@users.noreply.github.com>
2023-05-28 22:11:38 +0900

    feature: comment grammar

---
+++ easy-rust/src/comment.rs
@@ -0,0 +1,9 @@
+/// documentation type comments ex, (Book으로 쓰는 타입이다.)
+struct Book;
+fn comment() {
+    //  ( // ) mean is normal type comments
+    println!("Hello, world!");
+    /*중간에 넣을 수 있는 주석*/
+    let _my_number = 10; // if you insert _ front of variable , compiler dismiss in warning message
+    println!(_my_number)
+}
