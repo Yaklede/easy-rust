18e288fddbcdb1dfb762ea93b511ea5712be8c6c
Yaklede <68309983+Yaklede@users.noreply.github.com>
2023-05-30 19:29:46 +0900

    fix: test main

--- easy-rust/src/main.rs
+++ easy-rust/src/main.rs
@@ -1,8 +1,16 @@
 mod number_type;
 mod char_and_string;
 mod print_line;
+mod semicolon;
+mod function;
+mod mutable_and_shadowing;
+mod fancy_printing;
 fn main() {
     number_type::number();
     char_and_string::text();
     print_line::print_line();
+    semicolon::semicolon();
+    function::function();
+    mutable_and_shadowing::mutable();
+    fancy_printing::fancy_print();
 }
