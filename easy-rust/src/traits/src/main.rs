71961c76dbb73ebb22888cf32bf6e652a878bb69
Yaklede <chh78151@naver.com>
2023-06-21 16:57:28 +0900

    feature: trait (3)

--- easy-rust/src/traits/src/main.rs
+++ easy-rust/src/traits/src/main.rs
@@ -2,6 +2,8 @@
 // it's very similar interface
 
 
+extern crate core;
+
 struct Animal {
     name: String
 }
@@ -27,6 +29,7 @@ impl Canine for Animal {
 }
 
 use std::fmt::{Display, Formatter};
+use std::ops::Range;
 
 struct Cat {
     name: String,
@@ -39,15 +42,60 @@ impl Display for Cat {
     }
 }
 
+struct Monster {
+    health: i32
+}
+
+struct Wizard {}
+
+struct Ranger {}
+
+trait FightClose {
+    fn attack_with_sword(&self, opponent: &mut Monster) {
+        opponent.health -= 10;
+        println!(" You strike with your sword! Your opponent's health is now  {}", opponent.health)
+    }
+    fn attack_with_hand(&self, opponent: &mut Monster) {
+        opponent.health -= 5;
+        println!(" You strike with your hand! Your opponent's health is now  {}", opponent.health)
+    }
+}
+
+trait FightFromDistance {
+    fn attack_with_bow(&self, opponent: &mut Monster, distance: u32) {
+        if distance < 10 {
+            opponent.health -= 10;
+            println!("YOu attack with your bow! Your opponent's health is {}", opponent.health);
+        }
+    }
+    fn attach_with_rock(&self, opponent: &mut Monster, distance: u32) {
+        if distance < 3 {
+            opponent.health -= 4;
+            println!("You attack with a rock your opponent's health {}", opponent.health);
+        }
+    }
+}
+
+impl FightFromDistance for Ranger {}
+impl FightClose for Wizard {}
+
 fn main() {
     // let animal = Animal {
     //     name: "my_dog".to_string()
     // };
     // animal.run();
     // animal.bark();
-    let my_cat = Cat {
-        name: "cat".to_string(),
-        age: 10
+    // let my_cat = Cat {
+    //     name: "cat".to_string(),
+    //     age: 10
+    // };
+    // println!("{my_cat}")
+    let radagast = Wizard {};
+    let aragorn = Ranger {};
+    let mut monster = Monster {
+        health: 50
     };
-    println!("{my_cat}")
+    radagast.attack_with_sword(&mut monster);
+    aragorn.attack_with_bow(&mut monster, 5);
+    println!("{}", monster.health);
 }
